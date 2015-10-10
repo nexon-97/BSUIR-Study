48c2bcb3a7b8bff2d8fe944c97a7917f2fbe68f7
nexon-97 <nexon.97@mail.ru>
2015-10-10 11:06:34 +0300

    Helphful String Functions

---
+++ Metrology/NexonStringUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Metrics {
+
+	public static class NexonStringUtils {
+
+		public static Boolean CompareStringOffsetToExample(
+			String ComparedString, Int32 Offset, String Example
+		) {
+			return (
+				(ComparedString.Length >= Offset + Example.Length) &&
+				(ComparedString.Substring(Offset, Example.Length).Equals(Example))
+			);
+		}
+
+	}
+
+}
